828e3405b5be47922af72ee8bb505122d8c5276e
JakubCzerwiec <jacob.czerwiec@gmail.com>
2022-02-17 19:13:26 +0100

    Calculator test

---
+++ calculator.js
@@ -0,0 +1,10 @@
+
+
+const calculator = {
+    add: (x, y) => x + y,
+    substr: (x, y) => x - y,
+    multipla: (x, y) => x * y,
+    divide: (x, y) => x / y
+}
+
+module.exports = calculator
---
+++ calculator.test.js
@@ -0,0 +1,18 @@
+const { multipla } = require('./calculator');
+const calculator = require('./calculator');
+
+it('adding', () => 
+    expect(calculator.add(3, 4)).toBe(7)
+)
+
+it('substracting', () => {
+    expect(calculator.substr(7, 1)).toBe(6)
+})
+
+it('multipla', () => {
+    expect(calculator.multipla(3, 5)).toBe(15)
+})
+
+it('dividing', () => {
+    expect(calculator.divide(15, 3)).toBe(5)
+})
